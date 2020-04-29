bd6ba6293a9e0abf724681040eb06ae80d34d4e7
BLipsett <53913632+BLipsett@users.noreply.github.com>
2020-04-28 20:02:54 -0400

    heroku pass

--- config/connection.js
+++ config/connection.js
@@ -11,7 +11,7 @@ let connection = mysql.createConnection({
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: "eatdaburger"
+    database: "w4i75a7f5k22uwgh"
     
 
     
